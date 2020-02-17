a9d92cde2c58ad32b12df07fcdc9c15dca51a1e4
Alexey <alexe@gmail.com>
2020-02-17 17:42:08 +0700

    echo

--- application/views/templates/main_page.php
+++ application/views/templates/main_page.php
@@ -17,7 +17,21 @@
                     <h3>Здравствуй, пользователь</h3>
                 </div>
 
+                <form id="form-buttons" method="post" action="/main">
+                    <input type="submit" value="Главная">
+                </form>
 
+                <form id="form-buttons" method="post" action="/service">
+                    <input type="submit" value="Услуги">
+                </form>
+
+                <form id="form-buttons" method="post" action="/portfolio">
+                <input type="submit" value="Портфолио">
+                </form>
+
+                <form id="form-buttons" method="post" action="/contacts">
+                <input type="submit" value="Контакты">
+                </form>
 
             </div>
             <div class="main-template">
