ce220469480fa8daa4ee82db93553386bb4fc4d0
Alexey <alexe@gmail.com>
2020-02-17 11:01:16 +0700

    echo

---
+++ application/views/templates/third.php
@@ -0,0 +1,8 @@
+<div style="display:grid; grid-row-gap: 50px;">
+    <h1>Это страница, №3</h1>
+    <p style="height: 100px; width: 400px;">
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat.
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+        sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</div>
